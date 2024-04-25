3099b250b6d5b9a9ab3973459b9b68cc41fb7224
ZhangQingsen <1463769805zqs@gmail.com>
2024-04-25 11:43:39 -0400

    some data with feature

--- code/util.py
+++ code/util.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def split_dataframe(df, train_ratio):
     # Ensure the ratios < 1
     assert train_ratio < 1, "Ratios must less than 1"
@@ -10,14 +12,10 @@ def split_dataframe(df, train_ratio):
 
     return train_df, test_df
 
-def binarize_pred(y_test, y_pred, binary=False):
-    # Binarize the pred to -1 and 1
-    # then times y_test
-    # # bin_arr = np.array(list(map(lambda x: -1 if x>0 else 1, y_pred)))
-    # bin_arr = y_pred if binary else np.array(list(map(lambda x: -1 if x<0 else 1, y_pred)))
-    y_pred = pd.Series(y_pred, index=y_test.index)
-    bin_arr = y_pred if binary else y_pred.apply(lambda x : 1 if x>=0 else -1)
-    return bin_arr * y_test
+def binarize_y(y):
+    # Binarize the y to -1 and 1
+    bin_arr =  y.apply(lambda x : 1 if x>=0 else -1)
+    return bin_arr
 
 
 def ff2(df):
@@ -28,5 +26,52 @@ def ff3(df):
     # plot the signal compare
     return
 
-def sharp_ratio(df):
-    return
+def calculate_daily_risk_free_log_return(risk_free_annual_rate=0.02, trading_days=252):
+    daily_rate = risk_free_annual_rate / trading_days
+    daily_log_return = np.log(1 + daily_rate)
+    return daily_log_return
+
+def calculate_sharpe_ratio(returns, risk_free_rate=calculate_daily_risk_free_log_return()):
+    """
+    Calculate the Sharpe ratio of a portfolio.
+    
+    Parameters:
+    - returns: A pandas Series representing the returns of the portfolio.
+    - risk_free_rate: The risk-free rate of return.
+    
+    Returns: The Sharpe ratio.
+    """
+    excess_returns = returns - risk_free_rate
+    sharpe_ratio = np.mean(excess_returns) / np.std(excess_returns)
+    return sharpe_ratio
+
+def calculate_treynor_ratio(returns, risk_free_rate, beta):
+    """
+    Calculate the Treynor ratio of a portfolio.
+    
+    Parameters:
+    - returns: A pandas Series representing the returns of the portfolio.
+    - risk_free_rate: The risk-free rate of return.
+    - beta: The beta of the portfolio.
+    
+    Returns: The Treynor ratio.
+    """
+    excess_returns = returns - risk_free_rate
+    treynor_ratio = np.mean(excess_returns) / beta
+    return treynor_ratio
+
+def calculate_jensens_alpha(returns, market_returns, risk_free_rate, beta):
+    """
+    Calculate Jensen's Alpha of a portfolio.
+    
+    Parameters:
+    - returns: A pandas Series representing the returns of the portfolio.
+    - market_returns: A pandas Series representing the returns of the market.
+    - risk_free_rate: The risk-free rate of return.
+    - beta: The beta of the portfolio.
+    
+    Returns: Jensen's Alpha.
+    """
+    expected_returns = risk_free_rate + beta * (market_returns - risk_free_rate)
+    jensens_alpha = np.mean(returns - expected_returns)
+    return jensens_alpha
